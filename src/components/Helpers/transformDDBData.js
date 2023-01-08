dbf0d6c2e1515a19d56a9d44561f9b5be807cca5
Sam Hoare <hoarus@gmail.com>
2023-01-08 16:13:25 -0700

    Sort data retrieved from stat table prior to displaying bar graph

--- src/components/Helpers/transformDDBData.js
+++ src/components/Helpers/transformDDBData.js
@@ -11,12 +11,56 @@ function convertDDBResponse(array) {
       hour: obj.HOUR.S,
       max: obj.MAX.N,
       maxHourDisplay: obj.MAXHOURDISPLAY.S,
-      min: obj.MIN.S,
+      min: obj.MIN.N,
       minHourDisplay: obj.MINHOURDISPLAY.S,
-      month: obj.MONTH.s,
+      month: obj.MONTH.S,
     })
   }
-  return convertedArray
+
+
+  function determineHourNumberOnly(hour) {
+    if(hour.length == 4) {
+      return hour.slice(0, 2)
+    } else {
+      return hour.slice(0, 1)
+    }
+  }
+
+  function sortArray(array) {
+    array.sort((a, b) => {
+    
+      return a.hourNumberOnly - b.hourNumberOnly
+    })
+
+    // Reorganize to place 12am/pm at the start of the array
+    let hourTwelve = array.pop();
+    array.unshift(hourTwelve);
+
+  }
+  function sortConvertedArray(array) {
+    let amArray = [];
+    let pmArray = [];
+  
+    for (let obj of array) {
+      let meridiem = obj.hour.slice(-2);
+      obj.hourNumberOnly = Number(determineHourNumberOnly(obj.hour));
+      if (meridiem == "am") {
+        amArray.push(obj);
+      } else {
+        pmArray.push(obj);
+      }
+    }
+    sortArray(amArray);
+    sortArray(pmArray);
+    return (amArray.concat(pmArray));
+
+  }
+
+
+
+ let sortedArray = sortConvertedArray(convertedArray)
+
+  return sortedArray
 }
 
 
